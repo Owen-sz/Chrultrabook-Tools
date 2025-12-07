8e9f4cfe59c07763473afa21e248d4fc1b13f710
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2025-12-07 21:22:15 +0530

    add a check for rgb keyboards

--- src/app/keyboard-extra/keyboard-extra.component.ts
+++ src/app/keyboard-extra/keyboard-extra.component.ts
@@ -1,40 +1,58 @@
 import { Component } from "@angular/core";
 import { RouterLink, RouterOutlet } from "@angular/router";
+import { invoke } from "@tauri-apps/api/core";
 
 
 @Component({
-    selector: "app-keyboard-extra",
-    standalone: true,
-    imports: [RouterLink, RouterOutlet],
-    templateUrl: "./keyboard-extra.component.html",
-    styleUrl: "./keyboard-extra.component.scss"
+  selector: "app-keyboard-extra",
+  standalone: true,
+  imports: [RouterLink, RouterOutlet],
+  templateUrl: "./keyboard-extra.component.html",
+  styleUrl: "./keyboard-extra.component.scss",
 })
 export class KeyboardExtraComponent {
-    remap: string = "active";
-    rgb_class: string = "";
+  remap: string = "active";
+  rgb_class: string = "";
 
-    current_title: string = 'Keyboard Remapping';
-    current_img: string = '../../assets/keyboard/keyboard.svg';
+  current_title: string = "Keyboard Remapping";
+  current_img: string = "../../assets/keyboard/keyboard.svg";
 
-    removeActive() {
+  rgb_enabled: boolean = true;
+
+  ngOnInit()
+  {
+    invoke("execute", {
+      program: "ectool",
+      arguments: ["rgbkbd", "getconfig"],
+      reply: true,
+    }).then((event) => {
+        let output: any = event;
+        let split = output.split(" ");
+
+        if(split[0] == 'Ec')
+        {
+            this.rgb_enabled = false;
+        }
+
+    })
+  }
+
+  removeActive() {
     this.remap = " ";
     this.rgb_class = " ";
   }
 
-    keyboard_remap()
-    {
-        this.removeActive();
-        this.remap = "active";
-        this.current_title = 'Keyboard Remapping';
-        this.current_img = '../../assets/keyboard/keyboard.svg';
-    }
+  keyboard_remap() {
+    this.removeActive();
+    this.remap = "active";
+    this.current_title = "Keyboard Remapping";
+    this.current_img = "../../assets/keyboard/keyboard.svg";
+  }
 
-    rgb()
-    {
-        this.removeActive();
-        this.rgb_class = "active";
-        this.current_title = "RGB Lighting Control";
-        this.current_img = "../../assets/activity_light/activity.svg";
-    }
-    
+  rgb() {
+    this.removeActive();
+    this.rgb_class = "active";
+    this.current_title = "RGB Lighting Control";
+    this.current_img = "../../assets/activity_light/activity.svg";
+  }
 }
